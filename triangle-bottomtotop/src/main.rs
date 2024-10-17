adac3a85246904ef68912a11ebfd6ff536801304
Mohan Raj <mohanzolomon@gmail.com>
2024-10-17 18:05:06 +0530

    triangle bottom to top

---
+++ triangle-bottomtotop/src/main.rs
@@ -0,0 +1,27 @@
+fn get_num_from_io(prompt: &str) -> u32 {
+    let mut text = String::from("");
+
+    println!("{}", prompt);
+
+    std::io::stdin()
+        .read_line(&mut text)
+        .expect("Something went wrong during I/O.");
+
+    let num = text
+        .trim()
+        .parse::<u32>()
+        .expect("Something went wrong during I/O parsing.");
+
+    return num;
+}
+
+fn main() {
+    let num: u32 = get_num_from_io("Enter number:");
+
+    for i in 0..num {
+        println!(
+            "{}",
+            (0..(num - i)).fold(String::new(), |acc, el| format!("{acc}*"))
+        );
+    }
+}
