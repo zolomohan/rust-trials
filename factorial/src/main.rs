5bfbd5c85517af991f39f01338a13a55df7d71b7
Mohan Raj <mohanzolomon@gmail.com>
2024-10-17 15:54:31 +0530

    cleanup

--- factorial/src/main.rs
+++ factorial/src/main.rs
@@ -1,34 +1,30 @@
 mod humanize;
 use num_bigint::BigUint;
 
-fn factorial_bigint(number: usize) -> BigUint {
-    let mut result: BigUint = BigUint::new(vec![1]);
+fn factorial_bigint(num: usize) -> BigUint {
+    let mut res: BigUint = BigUint::new(vec![1]);
 
-    for n in 0..number {
-        result = result * (number - n);
+    for i in 0..num {
+        res = res * (num - i);
     }
 
-    return result;
+    return res;
 }
 
 fn main() {
-    let mut user_input: String = String::new();
+    let mut input: String = String::new();
     println!("Enter number to calculate factorial: ");
 
     std::io::stdin()
-        .read_line(&mut user_input)
+        .read_line(&mut input)
         .expect("Something went wrong during input.");
 
-    let number: usize = user_input
+    let num: usize = input
         .trim()
         .parse::<usize>()
         .expect("Something went wrong during parsing input.");
 
-    let result: BigUint = factorial_bigint(number);
+    let res: BigUint = factorial_bigint(num);
 
-    println!(
-        "The factorial of {} is {}",
-        number,
-        humanize::big_int(&result)
-    );
+    println!("The factorial of {} is {}", num, humanize::big_int(&res));
 }
